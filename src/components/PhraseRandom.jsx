78cdd47f384721b3145e78acda77caae4c2f7f7a
akani28 <nietocevallos61@gmail.com>
2023-10-11 01:46:24 -0500

    upload files in my proyect

--- src/components/PhraseRandom.jsx
+++ src/components/PhraseRandom.jsx
@@ -1,32 +1,32 @@
-import React from 'react'
+import React from "react";
 
-import { getData } from '../dataBase'
+import { getData } from "../dataBase";
 
+const PhraseRandom = ({ setPhrase }) => {
+  const getRandomPhrase = () => {
+    setPhrase(getData()?.phrase);
+  };
 
-const PhraseRandom = ({setPhrase}) => {
-
-    const getRandomPhrase=()=>{
-
-        setPhrase(getData()?.phrase)
-
-    }
-    
-
-    
   return (
-    <div className='content_button'>
+    <div className="content_button">
       <section>
-      <button className='btn' onClick={getRandomPhrase}>See Other</button>
+        <button className="btn" onClick={getRandomPhrase}>
+          See Other
+        </button>
       </section>
-    
-    <section>
-      <section className='content-image'>
-        <img className="image-logo" src="images/logocookies.png" alt="logo empanadas" />
-      </section>
-      <p>{getData()?.author}</p>
-    </section>
-    </div>
-  )
-}
 
-export default PhraseRandom
+      <section>
+        <section className="content-image">
+          <img
+            className="image-logo"
+            src="images/logocookies.png"
+            alt="logo empanadas"
+          />
+        </section>
+        <p>{getData()?.author}</p>
+      </section>
+    </div>
+  );
+};
+
+export default PhraseRandom;
